e2307afde08dd8d0164ce675887934f84f46cf43
Kamila Klassen <kamilkavltv@icloud.com>
2021-03-06 16:19:39 +0100

    Make methods final, add protected abstract method insert

--- src/com/basejava/webapp/storage/AbstractArrayStorage.java
+++ src/com/basejava/webapp/storage/AbstractArrayStorage.java
@@ -6,21 +6,28 @@ import java.util.Arrays;
 
 public abstract class AbstractArrayStorage implements Storage {
     protected static final int STORAGE_LIMIT = 10_000;
-
     protected Resume[] storage = new Resume[STORAGE_LIMIT];
     protected int actualSize = 0;
 
-    protected abstract int getIndex(String uuid);
+    public final void save(Resume resume) {
+        int index = getIndex(resume.getUuid());
+        if (actualSize == STORAGE_LIMIT) {
+            System.out.println("The storage is full!");
+            return;
+        }
+        if (index >= 0) {
+            System.out.println("The resume " + resume.getUuid() + " already exists!");
+            return;
+        }
+        insert(resume, index);
+        actualSize++;
+    }
 
-    public abstract void delete(String uuid);
-
-    public abstract void save(Resume resume);
-
-    public int size() {
+    public final int size() {
         return actualSize;
     }
 
-    public Resume get(String uuid) {
+    public final Resume get(String uuid) {
         int index = getIndex(uuid);
         if (index == -1) {
             System.out.println("The resume " + uuid + " does not exist!");
@@ -29,7 +36,7 @@ public abstract class AbstractArrayStorage implements Storage {
         return storage[index];
     }
 
-    public void update(Resume resume) {
+    public final void update(Resume resume) {
         int index = getIndex(resume.getUuid());
         if (index >= 0) {
             storage[index] = resume;
@@ -38,12 +45,29 @@ public abstract class AbstractArrayStorage implements Storage {
         }
     }
 
-    public void clear() {
+    public final void delete(String uuid) {
+        int index = getIndex(uuid);
+        if (index >= 0) {
+            for (int i = index + 1; i < actualSize; i++) {
+                storage[i - 1] = storage[i];
+            }
+            storage[actualSize - 1] = null;
+            actualSize--;
+        } else {
+            System.out.println("The resume " + uuid + " does not exist!");
+        }
+    }
+
+    public final void clear() {
         Arrays.fill(storage, 0, actualSize, null);
         actualSize = 0;
     }
 
-    public Resume[] getAll() {
+    public final Resume[] getAll() {
         return Arrays.copyOfRange(storage, 0, actualSize);
     }
+
+    protected abstract int getIndex(String uuid);
+
+    protected abstract void insert(Resume resume, int index);
 }
