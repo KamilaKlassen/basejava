bbbe972077cb2ba6741dfc7fe55d77fdd37c5b5c
Kamila Klassen <kamilkavltv@icloud.com>
2021-03-12 16:17:19 +0100

    Add test classes and MainReflection

---
+++ test/com/basejava/webapp/storage/ArrayStorageTest.java
@@ -0,0 +1,24 @@
+package com.basejava.webapp.storage;
+
+import com.basejava.webapp.model.Resume;
+import org.junit.Before;
+
+public class ArrayStorageTest extends AbstractArrayStorageTest {
+    private ArrayStorage storage;
+
+    private static final String UUID_1 = "uuid1";
+    private static final String UUID_2 = "uuid2";
+    private static final String UUID_3 = "uuid3";
+
+    public ArrayStorageTest(ArrayStorage storage) {
+        super(storage);
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        storage.clear();
+        storage.save(new Resume(UUID_1));
+        storage.save(new Resume(UUID_2));
+        storage.save(new Resume(UUID_3));
+    }
+}
