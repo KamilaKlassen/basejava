79203dff1a61acfd0c7ff490bd28bb28cbf7be5e
Kamila Klassen <kamilkavltv@icloud.com>
2021-03-17 12:46:53 +0100

    Add AbstractArrayStorageTest, move saveStorageOverflow test

---
+++ test/com/basejava/webapp/storage/AbstractArrayStorageTest.java
@@ -0,0 +1,25 @@
+package com.basejava.webapp.storage;
+
+import com.basejava.webapp.exception.StorageException;
+import com.basejava.webapp.model.Resume;
+import org.junit.Assert;
+import org.junit.Test;
+
+public abstract class AbstractArrayStorageTest extends AbstractStorageTest {
+
+    protected AbstractArrayStorageTest(Storage storage) {
+        super(storage);
+    }
+
+    @Test(expected = StorageException.class)
+    public void saveStorageOverflow() throws Exception {
+        try {
+            for (int i = 4; i < AbstractArrayStorage.STORAGE_LIMIT + 1; i++) {
+                storage.save(new Resume());
+            }
+        } catch (StorageException e) {
+            Assert.fail("Overflow occurred earlier than expected");
+        }
+        storage.save(new Resume());
+    }
+}
