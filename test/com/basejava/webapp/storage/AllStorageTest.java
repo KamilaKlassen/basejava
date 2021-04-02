aea1a9b3a9d96b26d9246a3ee1ca276d0d4f805a
Kamila Klassen <kamilkavltv@icloud.com>
2021-04-02 11:46:15 +0200

    Add MapResumeStorageTest to AllStorageTest

--- test/com/basejava/webapp/storage/AllStorageTest.java
+++ test/com/basejava/webapp/storage/AllStorageTest.java
@@ -10,9 +10,8 @@ import org.junit.runners.Suite;
                     SortedArrayStorageTest.class,
                     ListStorageTest.class,
                     MapUuidStorageTest.class,
-                    MapUuidStorageTest.class,
+                    MapResumeStorageTest.class,
             })
     public class AllStorageTest {
-
     }
 
