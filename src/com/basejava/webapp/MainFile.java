67914334084de8c2721a46c2010b135a432c1145
Kamila Klassen <kamilkavltv@icloud.com>
2021-04-04 13:58:35 +0200

    Add MainFile

---
+++ src/com/basejava/webapp/MainFile.java
@@ -0,0 +1,56 @@
+package com.basejava.webapp;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class MainFile {
+    public static void main(String[] args) {
+
+        String filePath = ".\\.gitignore";
+
+        File file = new File(filePath);
+        try {
+            System.out.println(file.getCanonicalPath());
+        } catch (IOException e) {
+            throw new RuntimeException("Error", e);
+        }
+
+        File dir = new File("./src/ru/javawebinar/basejava");
+        System.out.println(dir.isDirectory());
+        String[] list = dir.list();
+        if (list != null) {
+            for (String name : list) {
+                System.out.println(name);
+            }
+        }
+
+        try (FileInputStream fis = new FileInputStream(filePath)) {
+            System.out.println(fis.read());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        String myPath = "/Users/kamilaklassen/Desktop/BaseJava/basejava";
+        printAllFiles(myPath);
+    }
+
+    //List all files from a directory recursively
+    public static void printAllFiles(String path) {
+        File root = new File(path);
+        File[] list = root.listFiles();
+
+        if (list == null) {
+            return;
+        }
+        for (File f : list) {
+            if (f.isDirectory()) {
+                printAllFiles(f.getAbsolutePath());
+                System.out.println("Directory:" + f.getAbsoluteFile());
+            } else {
+                System.out.println("File:" + f.getAbsoluteFile());
+            }
+        }
+    }
+}
+
