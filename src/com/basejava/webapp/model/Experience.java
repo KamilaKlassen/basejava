a24fe5facab8fbae6228db4eab55fe89ac9d5ee2
Kamila Klassen <kamilkavltv@icloud.com>
2021-04-09 07:54:09 +0200

    Add varargs constructor, make Position inner class of Experience

--- src/com/basejava/webapp/model/Experience.java
+++ src/com/basejava/webapp/model/Experience.java
@@ -1,15 +1,27 @@
 package com.basejava.webapp.model;
 
+import com.basejava.webapp.util.DateUtil;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
+
+import static com.basejava.webapp.util.DateUtil.NOW;
 
 public class Experience {
 
     private final Link link;
-    private List<Position> positionList;
+    private List<Position> positionList = new ArrayList<>();
 
-    public Experience(String name, String url, List<Position> positionList) {
-        this.link = new Link(name, url);
+    public Experience(String name, String url, Position... positions) {
+        this(new Link(name, url), Arrays.asList(positions));
+    }
+
+    public Experience(Link link, List<Position> positionList) {
+        this.link = link;
         this.positionList = positionList;
     }
 
@@ -37,5 +49,66 @@ public class Experience {
     @Override
     public String toString() {
         return link + " " + positionList;
+    }
+
+    public static class Position {
+
+        private final LocalDate startDate;
+        private final LocalDate endDate;
+        private final String title;
+        private final String description;
+
+        public Position(int startYear, Month startMonth, String title, String description) {
+            this(DateUtil.of(startYear, startMonth), NOW, title, description);
+        }
+
+        public Position(int startYear, Month startMonth, int endYear, Month endMonth, String title, String description) {
+            this(DateUtil.of(startYear, startMonth), DateUtil.of(endYear, endMonth), title, description);
+        }
+
+        public Position(LocalDate startDate, LocalDate endDate, String title, String description) {
+            Objects.requireNonNull(startDate, "FromDate cannot be null");
+            Objects.requireNonNull(endDate, "ToDate cannot be null");
+            Objects.requireNonNull(title, "Title cannot be null");
+            this.startDate = startDate;
+            this.endDate = endDate;
+            this.title = title;
+            this.description = description;
+        }
+
+        public LocalDate getStartDate() {
+            return startDate;
+        }
+
+        public LocalDate getEndDate() {
+            return endDate;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Position position = (Position) o;
+            return startDate.equals(position.startDate) && endDate.equals(position.endDate) && title.equals(position.title)
+                    && Objects.equals(description, position.description);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(startDate, endDate, title, description);
+        }
+
+        @Override
+        public String toString() {
+            return startDate + " - " + endDate + " " + title + ": " + description;
+        }
     }
 }
