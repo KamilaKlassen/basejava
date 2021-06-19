fbd69124870039035527e0e954f528bcd760fa51
Kamila Klassen <76117697+KamilaKlassen@users.noreply.github.com>
2021-06-19 22:51:29 +0200

    Add database credentials

--- src/com/basejava/webapp/Config.java
+++ src/com/basejava/webapp/Config.java
@@ -10,8 +10,10 @@ public class Config {
     private static final File PROPS = new File("/Users/kamilaklassen/Desktop/BaseJava/basejava/config/resumes.properties");
     private static final Config INSTANCE = new Config();
 
-    private Properties props = new Properties();
-    private File storageDir;
+    private final File storageDir;
+    private final String dburl;
+    private final String dbuser;
+    private final String dbpasswort;
 
     public static Config get() {
         return INSTANCE;
@@ -19,8 +21,12 @@ public class Config {
 
     private Config() {
         try (InputStream is = new FileInputStream(PROPS)) {
+            Properties props = new Properties();
             props.load(is);
             storageDir = new File(props.getProperty("storage.dir"));
+            dburl = props.getProperty("db.url");
+            dbuser = props.getProperty("db.user");
+            dbpasswort = props.getProperty("db.password");
         } catch (IOException e) {
             throw new IllegalStateException("Invalid config file " + PROPS.getAbsolutePath());
         }
@@ -28,5 +34,17 @@ public class Config {
 
     public File getStorageDir() {
         return storageDir;
+    }
+
+    public String getDburl() {
+        return dburl;
+    }
+
+    public String getDbuser() {
+        return dbuser;
+    }
+
+    public String getDbpasswort() {
+        return dbpasswort;
     }
 }
