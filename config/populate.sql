672dc13b91ac4daebae39ac057559915e3f1972b
Kamila Klassen <76117697+KamilaKlassen@users.noreply.github.com>
2021-06-25 13:42:53 +0200

    Add contacts to save() and get()

--- config/populate.sql
+++ config/populate.sql
@@ -1,4 +1,4 @@
 INSERT INTO resume (uuid, full_name)
-VALUES ('7de882da-02f2-4d16-8daa-60660aaf4071', 'Name1'),
-       ('a97b3ac3-3817-4c3f-8a5f-178497311f1d', 'Name2'),
-       ('dd0a70d1-5ed3-479a-b452-d5e04f21ca73', 'Name3');
+VALUES ('325d5d23-f140-4c49-a911-716f29be9b86', 'Name1'),
+       ('74065c63-e63a-4731-bf84-4ced51fadfbd', 'Name2'),
+       ('34f06ae2-67ae-4916-a255-80ea2d728e93', 'Name3');
