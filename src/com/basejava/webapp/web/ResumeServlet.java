da5d5a9fbceaf77cc0c1dad16b454a069737d67d
Kamila Klassen <76117697+KamilaKlassen@users.noreply.github.com>
2021-07-15 11:56:21 +0200

    Add html for resume table into servlet

--- src/com/basejava/webapp/web/ResumeServlet.java
+++ src/com/basejava/webapp/web/ResumeServlet.java
@@ -1,18 +1,49 @@
 package com.basejava.webapp.web;
 
+import com.basejava.webapp.Config;
+import com.basejava.webapp.storage.Storage;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.IOException;
+import java.io.Writer;
 
 public class ResumeServlet extends HttpServlet {
+
+    private final Storage storage = Config.get().getStorage();
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
         response.setContentType("text/html; charset=UTF-8");
 
-        String name = request.getParameter("name");
-        response.getWriter().write(name == null ? "Hello Resumes!" : "Hello " + name + '!');
+        Writer writer = response.getWriter();
+        writer.write(
+                """
+                        <!DOCTYPE html>
+                        <html>
+                        <head>
+                            <meta charset="UTF-8">
+                            <title>Список резюме</title>
+                            <style>
+                                table, th, td {
+                                    border: 1px solid black;
+                                    border-collapse: collapse;
+                                }
+
+                                th,td {
+                                    padding: 5px;
+                                }
+                            </style>
+                        </head>
+                        <body>
+                        <table>
+                            <tr>
+                                <th>Имя</th>
+                                <th>E-Mail</th>
+                            </tr>"""
+        );
     }
 
     @Override
