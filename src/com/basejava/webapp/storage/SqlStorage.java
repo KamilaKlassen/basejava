878c59cf81e1b53cd31f51d870a7c42d6624fc80
Kamila Klassen <76117697+KamilaKlassen@users.noreply.github.com>
2021-07-20 20:29:08 +0200

    Register driver

--- src/com/basejava/webapp/storage/SqlStorage.java
+++ src/com/basejava/webapp/storage/SqlStorage.java
@@ -15,6 +15,11 @@ public class SqlStorage implements Storage {
     public SqlHelper sqlHelper;
 
     public SqlStorage(String dbUrl, String dbUser, String dbPassword) {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
         sqlHelper = new SqlHelper(() -> DriverManager.getConnection(dbUrl, dbUser, dbPassword));
     }
 
