b391998572cb35c358817c34ddeee5af163bcaac
Kamila Klassen <76117697+KamilaKlassen@users.noreply.github.com>
2021-08-06 16:31:25 +0200

    Add test data

--- src/com/basejava/webapp/ResumeTestData.java
+++ src/com/basejava/webapp/ResumeTestData.java
@@ -14,7 +14,7 @@ public class ResumeTestData {
 
         resume.addContact(ContactType.MOBILE, "+7(921) 855-0482");
         resume.addContact(ContactType.SKYPE, "grigory.kislin");
-        resume.addContact(ContactType.MAIL,"gkislin@yandex.ru");
+        resume.addContact(ContactType.MAIL, "gkislin@yandex.ru");
         resume.addContact(ContactType.GITHUB, "github.com/gkislin");
 
         resume.addSection(SectionType.PERSONAL, new TextSection("Аналитический склад ума, сильная логика, креативность, инициативность. Пурист кода и архитектуры."));
@@ -28,15 +28,39 @@ public class ResumeTestData {
 
         resume.addSection(SectionType.EXPERIENCE,
                 new OrganizationSection(
-                        new Experience("Organization", null,
-                                new Experience.Position(2010, Month.JANUARY, "Position", "Description"),
-                                new Experience.Position(2005, Month.JANUARY, 2009, Month.JANUARY, "Position", "Description"))));
+                        new Experience("Luxoft", "https://luxoft.ru", new Experience.Position(2010, Month.DECEMBER,
+                                2012, Month.APRIL, "Luxoft (Deutsche Bank)", "Ведущий программист\n" +
+                                "Участие в проекте Deutsche Bank CRM (WebLogic, Hibernate, Spring, Spring MVC, SmartGWT, GWT, " +
+                                "Jasper, Oracle). Реализация клиентской и серверной части CRM. Реализация RIA-приложения для " +
+                                "администрирования, мониторинга и анализа результатов в области алгоритмического трейдинга. JPA, " +
+                                "Spring, Spring-MVC, GWT, ExtGWT (GXT), Highstock, Commet, HTML5.")),
+                        new Experience("Yota", "https://www.yota.ru/", new Experience.Position(2008, Month.JUNE,
+                                2010, Month.DECEMBER, "Yota", "Ведущий специалист\n" +
+                                "Дизайн и имплементация Java EE фреймворка для отдела \"Платежные Системы\" " +
+                                "(GlassFish v2.1, v3, OC4J, EJB3, JAX-WS RI 2.1, Servlet 2.4, JSP, JMX, JMS, Maven2). " +
+                                "Реализация администрирования, статистики и мониторинга фреймворка. " +
+                                "Разработка online JMX клиента (Python/ Jython, Django, ExtJS)")),
+                        new Experience("Enkata", "http://enkata.com/", new Experience.Position(2007, Month.MARCH,
+                                2008, Month.JUNE, "Enkata", "\tРазработчик ПО\n" +
+                                "Реализация клиентской (Eclipse RCP) и серверной (JBoss 4.2, Hibernate 3.0, Tomcat, JMS)" +
+                                " частей кластерного J2EE приложения (OLAP, Data mining)."))));
+
         resume.addSection(SectionType.EDUCATION,
                 new OrganizationSection(
-                        new Experience("Organisation", "URL",
-                                new Experience.Position(1996, Month.JANUARY, 2000, Month.JANUARY, "Title", null),
-                                new Experience.Position(2001, Month.MAY, 2005, Month.JANUARY, "Title", "Description")),
-                        new Experience("Organization", "URL")));
+                        new Experience("Заочная физико-техническая школа при МФТИ",
+                                "http://www.school.mipt.ru/", new Experience.Position(1984, Month.SEPTEMBER, 1987, Month.JUNE,
+                                "Заочная физико-техническая школа при МФТИ", "Закончил с отличием")),
+                        new Experience("Санкт-Петербургский национальный исследовательский университет ", "https://itmo.ru/ru/",
+                                new Experience.Position(1993, Month.SEPTEMBER,
+                                        1996, Month.JULY, "Санкт-Петербургский национальный исследовательский университет " +
+                                        "информационных технологий, механики и оптики", "Аспирантура (программист С, С++)"),
+                                new Experience.Position(1987, Month.SEPTEMBER, 1993, Month.JULY,
+                                        "Санкт-Петербургский национальный исследовательский университет информационных технологий, " +
+                                                "механики и оптики", "Инженер (программист Fortran, C)")),
+                        new Experience("Luxoft", "https://luxoft.ru", new Experience.Position(2011, Month.MARCH,
+                                2011, Month.APRIL, "Luxoft", "Курс \"Объектно-ориентированный анализ ИС. " +
+                                "Концептуальное моделирование на UML.\""))));
+
         return resume;
     }
 
